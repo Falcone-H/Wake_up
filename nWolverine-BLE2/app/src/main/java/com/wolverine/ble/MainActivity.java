7c8f440e12836c3bf2a409bb940a0c9759d8c528
Falcone-H <james_bond1@163.com>
2020-11-20 14:28:51 +0800

    魔改后的Demo

---
+++ nWolverine-BLE2/app/src/main/java/com/wolverine/ble/MainActivity.java
@@ -0,0 +1,451 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wolverine.ble;
+
+
+
+
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.util.Date;
+import com.wolverine.ble.UartService;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements RadioGroup.OnCheckedChangeListener, View.OnClickListener {
+    private static final int REQUEST_SELECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    private static final int UART_PROFILE_READY = 10;
+    public static final String TAG = "nRFUART";
+    private static final int UART_PROFILE_CONNECTED = 20;
+    private static final int UART_PROFILE_DISCONNECTED = 21;
+    private static final int STATE_OFF = 10;
+
+    TextView mRemoteRssiVal;
+    RadioGroup mRg;
+    private String cur_message = null;
+    private int mState = UART_PROFILE_DISCONNECTED;
+    private UartService mService = null;
+    private BluetoothDevice mDevice = null;
+    private BluetoothAdapter mBtAdapter = null;
+    private ListView messageListView;
+    private ArrayAdapter<String> listAdapter;
+    private Button btnConnectDisconnect;
+    private Button btn_heart;
+    private Button btn_oxygen;
+    private Button btn_pressure;
+    private Button btn_sugar;
+    private Button btn_step;
+    private Button btn_calories;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        messageListView = (ListView) findViewById(R.id.listMessage);
+        listAdapter = new ArrayAdapter<String>(this, R.layout.message_detail);
+        messageListView.setAdapter(listAdapter);
+        messageListView.setDivider(null);
+        btnConnectDisconnect=(Button) findViewById(R.id.btn_select);
+
+        btn_heart = (Button) findViewById(R.id.btn_heart);
+        btn_oxygen = (Button) findViewById(R.id.btn_oxygen);
+        btn_pressure = (Button) findViewById(R.id.btn_pressure);
+        btn_sugar = (Button) findViewById(R.id.btn_sugar);
+        btn_step = (Button) findViewById(R.id.btn_step);
+        btn_calories = (Button) findViewById(R.id.btn_calories);
+
+        service_init();
+
+        btn_heart.setOnClickListener(this);
+        btn_oxygen.setOnClickListener(this);
+        btn_pressure.setOnClickListener(this);
+        btn_sugar.setOnClickListener(this);
+        btn_step.setOnClickListener(this);
+        btn_calories.setOnClickListener(this);
+     
+       
+        // Handler Disconnect & Connect button
+        btnConnectDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {   // Connect按钮
+                if (!mBtAdapter.isEnabled()) {
+                    Log.i(TAG, "onClick - BT not enabled yet");
+                    Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+                }
+                else {
+                	if (btnConnectDisconnect.getText().equals("Connect")){
+                		
+                		//Connect button pressed, open DeviceListActivity class, with popup windows that scan for devices
+                		
+            			Intent newIntent = new Intent(MainActivity.this, DeviceListActivity.class);
+            			startActivityForResult(newIntent, REQUEST_SELECT_DEVICE);
+        			} else {
+        				//Disconnect button pressed
+        				if (mDevice!=null)
+        				{
+        					mService.disconnect();
+        					
+        				}
+        			}
+                }
+            }
+        });
+        // Handler Send button
+
+     
+        // Set initial UI state
+        
+    }
+    
+    //UART service connected/disconnected
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder rawBinder) {
+        		mService = ((UartService.LocalBinder) rawBinder).getService();
+        		Log.d(TAG, "onServiceConnected mService= " + mService);
+        		if (!mService.initialize()) {
+                    Log.e(TAG, "Unable to initialize Bluetooth");
+                    finish();
+                }
+
+        }
+
+        public void onServiceDisconnected(ComponentName classname) {
+       ////     mService.disconnect(mDevice);
+        		mService = null;
+        }
+    };
+
+    private Handler mHandler = new Handler() {
+        @Override
+        
+        //Handler events that received from UART service 
+        public void handleMessage(Message msg) {
+  
+        }
+    };
+
+    private final BroadcastReceiver UARTStatusChangeReceiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            final Intent mIntent = intent;
+           //*********************//
+            if (action.equals(UartService.ACTION_GATT_CONNECTED)) {
+            	 runOnUiThread(new Runnable() {
+                     public void run() {
+                         	String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+                             Log.d(TAG, "UART_CONNECT_MSG");
+                             btnConnectDisconnect.setText("Disconnect");
+                             setEnabled(true);
+                             ((TextView) findViewById(R.id.deviceName)).setText(mDevice.getName()+ " - ready");
+                             listAdapter.add("["+currentDateTimeString+"] Connected to: "+ mDevice.getName());
+                        	 	messageListView.smoothScrollToPosition(listAdapter.getCount() - 1);
+                             mState = UART_PROFILE_CONNECTED;
+                     }
+            	 });
+            }
+           
+          //*********************//
+            if (action.equals(UartService.ACTION_GATT_DISCONNECTED)) {
+            	 runOnUiThread(new Runnable() {
+                     public void run() {
+                    	 	 String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+                             Log.d(TAG, "UART_DISCONNECT_MSG");
+                             btnConnectDisconnect.setText("Connect");
+                             setEnabled(false);
+                             ((TextView) findViewById(R.id.deviceName)).setText("Not Connected");
+                             listAdapter.add("["+currentDateTimeString+"] Disconnected to: "+ mDevice.getName());
+                             mState = UART_PROFILE_DISCONNECTED;
+                             mService.close();
+                            //setUiState();
+                         
+                     }
+                 });
+            }
+            
+          
+          //*********************//
+            if (action.equals(UartService.ACTION_GATT_SERVICES_DISCOVERED)) {
+             	 mService.enableTXNotification();
+            }
+          //*********************//
+            if (action.equals(UartService.ACTION_DATA_AVAILABLE)) {
+              
+                 final byte[] txValue = intent.getByteArrayExtra(UartService.EXTRA_DATA);
+                 runOnUiThread(new Runnable() {
+                     public void run() {
+                         try {
+                         	String text = new String(txValue, "UTF-8");
+                         	String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+                     		listAdapter.add("["+currentDateTimeString+"] "+cur_message +" : "+text);
+                    	 	messageListView.smoothScrollToPosition(listAdapter.getCount() - 1);
+                    	 	setEnabled(true);
+                         } catch (Exception e) {
+                             Log.e(TAG, e.toString());
+                         }
+                     }
+                 });
+             }
+           //*********************//
+            if (action.equals(UartService.DEVICE_DOES_NOT_SUPPORT_UART)){
+            	showMessage("Device doesn't support UART. Disconnecting");
+            	mService.disconnect();
+            }
+            
+            
+        }
+    };
+
+    private void service_init() {
+        Intent bindIntent = new Intent(this, UartService.class);
+        bindService(bindIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+  
+        LocalBroadcastManager.getInstance(this).registerReceiver(UARTStatusChangeReceiver, makeGattUpdateIntentFilter());
+    }
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(UartService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(UartService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(UartService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(UartService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(UartService.DEVICE_DOES_NOT_SUPPORT_UART);
+        return intentFilter;
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onDestroy() {
+    	 super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+        
+        try {
+        	LocalBroadcastManager.getInstance(this).unregisterReceiver(UARTStatusChangeReceiver);
+        } catch (Exception ignore) {
+            Log.e(TAG, ignore.toString());
+        } 
+        unbindService(mServiceConnection);
+        mService.stopSelf();
+        mService= null;
+       
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        if (!mBtAdapter.isEnabled()) {
+            Log.i(TAG, "onResume - BT not enabled yet");
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+ 
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+
+        case REQUEST_SELECT_DEVICE:
+        	//When the DeviceListActivity return, with the selected device address
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                String deviceAddress = data.getStringExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDevice = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(deviceAddress);
+               
+                Log.d(TAG, "... onActivityResultdevice.address==" + mDevice + "mserviceValue" + mService);
+                ((TextView) findViewById(R.id.deviceName)).setText(mDevice.getName()+ " - connecting");
+                mService.connect(deviceAddress);
+                            
+
+            }
+            break;
+        case REQUEST_ENABLE_BT:
+            // When the request to enable Bluetooth returns
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Bluetooth has turned on ", Toast.LENGTH_SHORT).show();
+
+            } else {
+                // User did not enable Bluetooth or an error occurred
+                Log.d(TAG, "BT not enabled");
+                Toast.makeText(this, "Problem in BT Turning ON ", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            break;
+        default:
+            Log.e(TAG, "wrong request code");
+            break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+       
+    }
+
+    
+    private void showMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+  
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mState == UART_PROFILE_CONNECTED) {
+            Intent startMain = new Intent(Intent.ACTION_MAIN);
+            startMain.addCategory(Intent.CATEGORY_HOME);
+            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startMain);
+            showMessage("nRFUART's running in background.\n             Disconnect to exit");
+        }
+        else {
+            new AlertDialog.Builder(this)
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .setTitle(R.string.popup_title)
+            .setMessage(R.string.popup_message)
+            .setPositiveButton(R.string.popup_yes, new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+   	                finish();
+                }
+            })
+            .setNegativeButton(R.string.popup_no, null)
+            .show();
+        }
+    }
+
+    /*
+        heart --- 心率
+        blo --- 血氧
+        blp --- 血压
+        bls --- 血糖
+        step --- 步伐
+        cal --- 卡路里
+         */
+    @Override
+    public void onClick(View view) {
+        String message = null;
+        switch(view.getId()) {
+            case R.id.btn_heart:
+                message = "heart";
+                break;
+            case R.id.btn_oxygen:
+                message = "blo";
+                break;
+            case R.id.btn_pressure:
+                message = "blp";
+                break;
+            case R.id.btn_sugar:
+                message = "bls";
+                break;
+            case R.id.btn_step:
+                message = "step";
+                break;
+            case R.id.btn_calories:
+                message = "cal";
+                break;
+        }
+        cur_message = message;
+        setEnabled(false);
+        byte[] value;
+        try {
+            //send data to service
+            value = message.getBytes("UTF-8");
+            mService.writeRXCharacteristic(value);
+            //Update the log with time stamp
+            String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+            listAdapter.add("["+currentDateTimeString+"] TX: "+ message);
+            messageListView.smoothScrollToPosition(listAdapter.getCount() - 1);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setEnabled(boolean flag) {
+        btn_heart.setEnabled(flag);
+        btn_oxygen.setEnabled(flag);
+        btn_pressure.setEnabled(flag);
+        btn_sugar.setEnabled(flag);
+        btn_step.setEnabled(flag);
+        btn_calories.setEnabled(flag);
+    }
+}
