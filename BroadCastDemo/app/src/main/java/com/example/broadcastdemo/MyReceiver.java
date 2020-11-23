593ebcd08ff3980e508804bcacfead6c354ad9e2
Falcone-H <james_bond1@163.com>
2020-11-23 15:30:37 +0800

    LocalBroadcast

---
+++ BroadCastDemo/app/src/main/java/com/example/broadcastdemo/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.example.broadcastdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("Main", "onReceive");
+        Toast.makeText(context, "received in MyReceiver", Toast.LENGTH_SHORT).show();
+    }
+}
